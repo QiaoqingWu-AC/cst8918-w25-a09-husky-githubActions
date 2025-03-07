bd76d8201e4f26aeb5e277d05b977f2df4f797cd
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-03-07 17:05:05 -0500

    Fix: Apply Terraform formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,15 +16,15 @@ provider "azurerm" {
 
 # Define the resource group in Azure
 resource "azurerm_resource_group" "rg" {
-  name = "${var.labelPrefix}-a09-rg"
+  name     = "${var.labelPrefix}-a09-rg"
   location = var.region
 }
 
 # Define a storage account
-resource "azurerm_storage_account" "storage" { 
-  name="${var.labelPrefix}a09storage"
-  resource_group_name=azurerm_resource_group.rg.name 
-  location=var.region 
-  account_tier="Standard" 
-  account_replication_type="LRS" 
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.labelPrefix}a09storage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.region
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
